d0963b0f27750bdfd0cea396357489d86720b881
Ruben1793 <ruben.ojed9a3@gmail.com>
2017-12-28 22:14:24 -0600

    Initial Commit
    Initial Commit

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(55)]
+        public String Name { get; set; }
+
+        
+        public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
+
+    }
+
+}
