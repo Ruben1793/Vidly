c9a6f0cf39ae89a1da1603c8020a067158ada851
Unknown <ruben.ojed9a3@gmail.com>
2018-02-12 20:39:53 -0600

    AutoMapper
    Lecture 68

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Dto;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+
+    }
+}
