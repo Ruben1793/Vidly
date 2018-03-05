624ef021c69f7799f849731cfbb09379a40892f5
Unknown <ruben.ojed9a3@gmail.com>
2018-03-04 19:27:36 -0600

    SQL changes in users and roles tables for authentication
    Part of lecture 93

---
+++ dbo.AspNetRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'47207b61-3037-4f5f-a674-0d5ba3ceb42c', N'CanManageMovie')
---
+++ dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'005e95d7-d24b-46ac-b540-e4af7daf7346', N'47207b61-3037-4f5f-a674-0d5ba3ceb42c')
