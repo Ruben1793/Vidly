feb9a1afce923eec94664ec442ae4204c285cfc3
Unknown <ruben.ojed9a3@gmail.com>
2018-03-11 13:26:00 -0600

    Add Phone to Aplication User
    Section 8 Exercise

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -8,6 +8,14 @@ namespace Vidly.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -65,13 +73,16 @@ namespace Vidly.Models
     public class RegisterViewModel
     {
         [Required]
-        [Display(Name = "Drving License")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Driving License")]
         public string DrivingLicense { get; set; }
 
         [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [StringLength(50)]
+        public string Phone { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
