aadabc56d4e91b647350515a5bec5f404df8a835
Unknown <ruben.ojed9a3@gmail.com>
2018-03-17 21:03:10 -0600

    Output Cache
    Lecture 104

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult Index()
         {
             return View();
