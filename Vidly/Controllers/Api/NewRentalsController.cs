26163b413c530379987077632fd62866eabb6b08
Unknown <ruben.ojed9a3@gmail.com>
2018-03-19 20:28:00 -0600

    API for movies rental

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Dto;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalsDto newRental)
+        {
+            //if (newRental.MovieIds.Count == 0)
+            //{
+            //    return BadRequest("No movies Ids have been given");
+            //}
+
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            //if (customer == null)
+            //{
+            //    return BadRequest("Customer Id is not valid");
+            //}
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            //if (movies.Count() != newRental.MovieIds.Count)
+            //{
+            //    return BadRequest("One or more Movie Ids are invalid");
+            //}
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available");
+                }
+                movie.NumberAvailable--;
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+                _context.Rentals.Add(rental);
+            }
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+
+}
